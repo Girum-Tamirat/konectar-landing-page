d001c704e40877294032c0c467e90cfc4ab89081
Girum-Tamirat <girumtamirat10@gmail.com>
2024-12-06 16:54:51 +0300

    deploy fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/konectar-landing-page',
   plugins: [react()],
+  base: '/konectar-landing-page/',
 })
